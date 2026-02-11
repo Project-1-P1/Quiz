927e980afb876f4becfb759dd1dda8dc2abd75ea
Project-1-P1 <project1.innovators@gmail.com>
2026-02-11 18:08:37 +0530

    Change Supabase URL and anon key
    Updated Supabase configuration with new URL and anon key.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /** 🚀 SUPABASE CONFIGURATION */
-const SUPABASE_URL = "https://hwllcgpdltcepztwefrm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_xAyGvh-d6HtVm_7ae12Duw_J1KkO-cV";
+const SUPABASE_URL = "https://avvjgnyfglnzwyexbzef.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_UBqcJJlB-BSdOkkzpib2Lw_WJAOc0Hl";
 
 // Initialize Supabase Client (Global Access)
 const { createClient } = supabase;

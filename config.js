e43bb8a75875fad69929c7d5014ade6e022b2113
Project-1-P1 <project1.innovators@gmail.com>
2026-02-15 20:59:58 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/** 🚀 SUPABASE CONFIGURATION */
-const SUPABASE_URL = "https://avvjgnyfglnzwyexbzef.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_UBqcJJlB-BSdOkkzpib2Lw_WJAOc0Hl";
-
-// Initialize Supabase Client (Global Access)
-const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
